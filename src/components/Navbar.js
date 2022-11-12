e181bf8a63a56ea5e0111a16a4efbcb9385366f4
MacFlyOSX <b.tasaki@gmail.com>
2022-11-11 23:38:34 -0800

    added navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,35 @@ const Navbar = () => {
     <div id='navbar-container'>
         <div id='navbar-inner'>
             <img id='navbar-logo' src={logo} alt='logo' />
+            <div id='navbar-middle'>
+                <a id='about-me-link' className='nav-mid-links' href="#about-me">{'< About Me >'}</a>
+                <a id='projects-link' className='nav-mid-links' href="#projects">{'< Projects >'}</a>
+            </div>
             <div id='navbar-right-section'>
-                <a href='https://github.com/MacFlyOSX/' rel="noopener noreferrer" target='_blank'>
+
+                <a className='nav-link' href='mailto: brandon.tasaki@gmail.com'>
+                    <div className='email'>
+                        <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                         width="58.877px" height="40px" viewBox="0 0 970.000000 659.000000"
+                         preserveAspectRatio="xMidYMid meet">
+                            <g id='email-g' transform="translate(0.000000,659.000000) scale(0.100000,-0.100000)"
+                            stroke="none">
+                            <path d="M994 6564 c-295 -49 -572 -219 -750 -460 -110 -147 -176 -297 -214
+                            -482 -20 -99 -20 -124 -20 -2327 0 -2203 0 -2228 20 -2327 27 -130 52 -202
+                            108 -312 172 -337 493 -571 865 -631 139 -23 7555 -23 7694 0 372 60 693 294
+                            865 631 56 110 81 182 108 312 20 99 20 124 20 2327 0 2203 0 2228 -20 2327
+                            -27 130 -52 202 -108 312 -143 279 -384 486 -680 585 -196 65 99 61 -4042 60
+                            -3119 -1 -3779 -3 -3846 -15z m6909 -772 c-53 -40 -3042 -1962 -3053 -1962
+                            -10 0 -3044 1951 -3064 1971 -6 5 1185 9 3065 9 2915 0 3073 -1 3052 -18z
+                            m-5049 -1746 c1945 -1250 1854 -1196 1996 -1196 142 0 40 -61 2131 1283 l1934
+                            1243 3 -2130 c2 -2364 8 -2170 -63 -2281 -55 -87 -127 -141 -224 -171 -69 -21
+                            -7493 -21 -7562 0 -97 30 -169 84 -224 171 -71 111 -65 -85 -65 2282 l0 2132
+                            138 -88 c75 -49 946 -609 1936 -1245z"/>
+                            </g>
+                        </svg>
+                    </div>
+                </a>
+                <a className='nav-link' href='https://github.com/MacFlyOSX/' rel="noopener noreferrer" target='_blank'>
                     <div className='github'>
                         <svg width="50px" height="50px" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path id='github-path' fillRule="evenodd" clipRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)"
